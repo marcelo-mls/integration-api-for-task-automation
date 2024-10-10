724ffaee95c1ca7763e703bc62aacfe819103235
Amanda Zanata <amandazanata46@gmail.com>
2024-10-10 12:23:12 -0300

    select jsons from dinamicJSON.js

--- src/utils/dinamicJSON.js
+++ src/utils/dinamicJSON.js
@@ -2,23 +2,47 @@ const { catalogJson } = require('../utils/catalogJSON');
 
 function createDedicatedJSON (data) {
   const selectedJson = catalogJson;
+  try {
+    if ( catalogJson == 'travaTelasHomeProd') {
+      selectedJson.payload.nomeOferta = data.nomeOferta;
+      selectedJson.payload.backgroundColor = data.backgroundColorA;
+      selectedJson.payload.imagemURL = data.figmaLinkA;
+      selectedJson.payload.corTitulo = data.colorTitleA;
+      selectedJson.payload.corValor = data.colorSubtitleA;
+      selectedJson.payload.acaoBotao = data.redirectionUrlA;
+      selectedJson.payload.temaBotao = data.colorCtaButtonA;
+      selectedJson.payload.corBotaoFechar = data.colorCloseButtonA;
+      selectedJson.payload.textoAcessibilidade = data.callToActionA; // para teste com o texto acessibilidade
+    }
+    
+    if ( catalogJson == 'dashResumo') {
+      selectedJson.payload.nomeOferta = data.nomeOferta;
+      selectedJson.payload.imagemURL = data.figmaLinkA;
+      selectedJson.payload.acao = data.redirectionUrlA;
+      selectedJson.payload.titulo = data.callToActionA;
+    }
 
-  if ( catalogJson == catalogJson.travaTelasHomeProd) {
-    selectedJson.payload.nomeOferta = selectedJson.payload.name = data.experienceNameA ? data.experienceNameA : selectedJson.payload.nomeOferta || 'Default Name';
-    selectedJson.payload.backgroundColor = data.backgroundColorA;
-    selectedJson.payload.imagemURL = data.figmaLinkA || null;
-    selectedJson.payload.imagemFullscreen = data.figmaLinkA ? null : (data.fullscreenBannerA || null);
-    selectedJson.payload.imagem = null;
-    selectedJson.payload.corTitulo = data.colorTitleA;
-    selectedJson.payload.corValor = data.colorSubtitleA;
-    selectedJson.payload.acaoBotao = data.redirectionUrlA;
-    selectedJson.payload.acao = data.redirectionUrlA;
-    selectedJson.payload.deeplink = data.redirectionUrlA;
-    selectedJson.payload.temaBotao = data.colorCtaButtonA;
-    selectedJson.payload.corBotaoFechar = data.colorCloseButtonA;
-    selectedJson.payload.textoAcessibilidade = data.callToActionA; // para teste com o texto acessibilidade
+    if ( catalogJson == 'homeResumo') {
+      selectedJson.payload.nomeOferta = data.nomeOferta;
+      selectedJson.payload.imagemURL = data.figmaLinkA;
+      selectedJson.payload.acao = data.redirectionUrlA;
+      selectedJson.payload.titulo = data.callToActionA;
+    }
+
+    if ( catalogJson == 'dashCartaoProd') {
+      selectedJson.payload.nomeOferta = data.nomeOferta;
+      selectedJson.payload.acao = data.redirectionUrlA;
+      selectedJson.payload.titulo = data.callToActionA;
+    }
+
+    if ( catalogJson == 'modalToast') {
+      selectedJson.payload.name = data.nomeOferta;
+      selectedJson.payload.deeplink = data.redirectionUrlA;
+      selectedJson.payload.title = data.callToActionA;
+    }
+  } catch (error) {
+    console.error(error);
   }
-
 }
 
 module.exports = {
